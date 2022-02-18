b2fb65e507f2a0055cce3e49dc8aff213fbf6575
emguse <30695541+emguse@users.noreply.github.com>
2022-02-18 20:28:48 +0900

    Add support for entry mode and fix command generation logic

--- circuit_aqm0802a.py
+++ circuit_aqm0802a.py
@@ -6,79 +6,67 @@ from adafruit_bus_device.i2c_device import I2CDevice
 print("Hello World!")
 
 '''
-- 2022/02/17 ver.0.02
+- 2022/02/18 ver.0.03
 - Author : emguse
 '''
 
-I2C_ADRESS=0x3e
-SET_IS_0 = bytes([0x00, 0x38])
-SET_IS_1 = bytes([0x00, 0x39])
-IOSC_FREQ = bytes([0x00, 0x14])
-INITIAL_CONT = 0x20
-FOLLOWER_CTL = bytes([0x00, 0x6C])
-CLEAR_DISP = bytes([0x00, 0x01])
-ENTRY_MODE = bytes([0x00, 0x07])
+_I2C_ADRESS=0x3e
+_FOLLOWER_CTL = 0x6C
+
+_CLEAR_DISP = 0x01
+_RETURN_HOME = 0x02
+
+_ENTRY_MODE_SET = 0x04
+
+_DISP_ON_OFF = 0x08
+_SET_CGRAM = 0x40
+_SET_DDRAM = 0x80
+
+_FUNCTION_SET = 0x20 # const
+_BIT_MODE_8 = 0x10 # const
+_NUM_OF_LINE_2 = 0x08 # const
+
+_IOSC_FREQ = 0x10
+_BIAS = 0x04
+_ADJ_IOSC_FREQ = 0x00
+
+_INITIAL_CONT = 0x20
 
 class CharacterLcdAqm0802():
     def __init__(self, i2c) -> None:
-        self.device = I2CDevice(i2c, I2C_ADRESS)
+        self.device = I2CDevice(i2c, _I2C_ADRESS)
         self.disp_on = True
         self.cursor_on = True
         self.cur_blink_on = True
-
-        self.set_is_0()
+        self.direction = 1
+        self.shift_entry = 0
+        self.screen_cursor_select = 0
+        
+        self.instruction_table = 0
+        self.init_display()
+    def init_display(self):
+        self.instruction_table = 0
+        self._function_set(self.instruction_table, _BIT_MODE_8, _NUM_OF_LINE_2)
         time.sleep(0.0000263)
-        self.set_is_1()
+        self.instruction_table = 1
+        self._function_set(self.instruction_table, _BIT_MODE_8, _NUM_OF_LINE_2)
         time.sleep(0.0000263)
-        self.set_iosc_freq()
+        self._set_iosc_freq()
         time.sleep(0.0000263)
-        self.set_contrast(INITIAL_CONT)
+        self._set_contrast(_INITIAL_CONT)
         time.sleep(0.0000263)
-        self.set_folower()
+        self._set_folower()
         time.sleep(0.2)
-        self.set_is_0()
+        self.instruction_table = 0
+        self._function_set(self.instruction_table, _BIT_MODE_8, _NUM_OF_LINE_2)
         time.sleep(0.0000263)
-        self.set_display_onoff()
+        self._set_display_onoff()
         time.sleep(0.0000263)
         self.clear_disp()
         time.sleep(0.108)
-    def set_is_0(self):
+    def _send_cmmand(self, com):
         with self.device:
-            self.device.write(SET_IS_0)
-    def set_is_1(self):
-        with self.device:
-            self.device.write(SET_IS_1)
-    def set_iosc_freq(self):
-        with self.device:
-            self.device.write(IOSC_FREQ)
-    def set_contrast(self, cont):
-        c1234 = cont & 0b00001111
-        contrast_set = 0b01110000 | c1234
-        c56 = (cont & 0b00110000) >> 4
-        pw_icon_cont = 0b01010100 | c56
-        with self.device:
-            self.device.write(bytes([0x00, contrast_set]))
-            time.sleep(0.0000263)
-            self.device.write(bytes([0x00, pw_icon_cont]))
-    def set_folower(self):
-        with self.device:
-            self.device.write(FOLLOWER_CTL)
-    def set_display_onoff(self):
-        cmd = 0b00001111
-        if self.disp_on == False:
-            cmd = cmd & 0b11111011
-        if self.cursor_on == False:
-            cmd = cmd & 0b11111101
-        if self.cur_blink_on == False:
-            cmd = cmd & 0b11111110
-        with self.device:
-            self.device.write(bytes([0x00, cmd]))
-    def clear_disp(self):
-        with self.device:
-            self.device.write(CLEAR_DISP)
-    def entry_mode(self):
-        with self.device:
-            self.device.write(ENTRY_MODE)
+            self.device.write(bytes([0x00, com]))
     def send_chr(self, s):
         self.cut_8(s)
         with self.device:
@@ -87,6 +75,49 @@ class CharacterLcdAqm0802():
     def cut_8(self, s):
         s = s[:8]
         return s
+    def _function_set(self, is_, bit, line):
+        self._send_cmmand(_FUNCTION_SET | is_ | bit | line)
+    def _set_iosc_freq(self):
+        self._send_cmmand(_IOSC_FREQ | _BIAS | _ADJ_IOSC_FREQ)
+    def _set_contrast(self, cont):
+        c1234 = cont & 0b00001111
+        contrast_set = 0b01110000 | c1234
+        c56 = (cont & 0b00110000) >> 4
+        pw_icon_cont = 0b01010100 | c56
+        self._send_cmmand(contrast_set)
+        time.sleep(0.0000263)
+        self._send_cmmand(pw_icon_cont)
+    def _set_folower(self):
+        self._send_cmmand(_FOLLOWER_CTL)
+    def _set_display_onoff(self):
+        cmd = 0b00001111
+        if self.disp_on == False:
+            cmd = cmd & 0b11111011
+        if self.cursor_on == False:
+            cmd = cmd & 0b11111101
+        if self.cur_blink_on == False:
+            cmd = cmd & 0b11111110
+        self._send_cmmand(cmd)
+    def clear_disp(self):
+        self._send_cmmand(_CLEAR_DISP)
+    def return_home(self):
+        self._send_cmmand(_RETURN_HOME)
+    def _entry_mode(self):
+        self._send_cmmand(_ENTRY_MODE_SET | (self.direction << 1) | self.shift_entry)
+    def _shift_mode(self):
+        self._send_cmmand()
+    def set_direction_left_to_right(self):
+        self.direction = 1
+        self._entry_mode()
+    def set_direction_right_to_left(self):
+        self.direction = 0
+        self._entry_mode()
+    def set_string_shift_left(self):
+        self.shift_entry = 1
+        self._entry_mode()
+    def set_string_shift_never(self):
+        self.shift_entry = 0
+        self._entry_mode()
 
 def main():
     i2c = busio.I2C(board.GP3, board.GP2)
@@ -94,9 +125,32 @@ def main():
     while True:
         print('Input string')
         s = input()
+        #print(bytearray(s))
+        #print(s == '@direction to left')
+        if s == '@cls':
+            lcd.clear_disp()
+            continue
+        elif s == '@init display':
+            lcd.init_display()
+            lcd.set_direction_left_to_right()
+            lcd.set_string_shift_left()
+            lcd.return_home
+            continue
+        elif s == '@direction to right':
+            lcd.set_direction_left_to_right()
+            continue
+        elif s == '@direction to left':
+            lcd.set_direction_right_to_left()
+            continue
+        elif s == '@string shift never':
+            lcd.set_string_shift_never()
+            continue
+        elif s == '@string shift left':
+            lcd.set_string_shift_left()
+            continue
+        #lcd.clear_disp()
         if len(s) > 8:
             s = lcd.cut_8(s)
-        lcd.clear_disp()
         lcd.send_chr(str(s))
 
 if __name__ == "__main__":
